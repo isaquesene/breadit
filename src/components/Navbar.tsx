9ea5559787358842dd6cd20b5f743ceeb9012797
isaque sene <isaqueconcursos26@gmail.com>
2023-10-21 19:03:43 -0300

    feat: button sign in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import {Icons} from "./Icons"
+import { buttonVariants } from "./ui/Button"
 
 const Navbar = () => {
     return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
@@ -13,6 +14,7 @@ const Navbar = () => {
             </Link>
 
             {/* search bar */}
+            <Link href='/sing-in' className={buttonVariants()}>Sign in</Link>
         </div>
     </div>
 }
